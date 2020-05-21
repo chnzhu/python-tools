27bc28a41ae8693f8eb791d33c84f730c2cbbcd3
chnzhu <33563118+chnzhu@users.noreply.github.com>
2020-05-21 09:33:44 +0800

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -3,7 +3,7 @@ import sys
 from ZSI.client import AUTH
 
 b=NPBinding(url='http://21.96.51.66:8080/axis/services/SendSms?wsdl',tracefile=sys.stdout)
-b.SetAuth(AUTH.httpbasic,'sendsms','zaq1xsw2')
-message='“¯’∆πÒ≤‚ ‘,2001.10'
+b.SetAuth(AUTH.httpbasic,'sendsms','')
+message='kkkkkkkkk'
 a=message.decode('GBK').encode('UTF-8')
-b.sendSms(phoneNumber='13551384548',message=a)
+b.sendSms(phoneNumber='1111111111',message=a)
