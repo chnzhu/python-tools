e95e30c1f9c3c8422725624b8c97b025e7d8e26e
chnzhu <33563118+chnzhu@users.noreply.github.com>
2020-05-21 09:24:24 +0800

    Add files via upload

---
+++ batch_rename.py
@@ -0,0 +1,17 @@
+import cx_Oracle
+conn=cx_Oracle.connect("newdss/yhnu67@query570")
+curs_table=conn.cursor()
+curs_col=conn.cursor()
+curs_table.execute("select table_name, table_type   from sys.all_catalog where owner = 'NEWDSS'  and \
+                   table_type = 'TABLE'   and table_name  like 'GYJX%'")
+ctlfile=open(r"c:\temp\ctl\batch_rename","w")
+for row_table in curs_table.fetchall():
+    curs_col.execute("select column_name,comments from all_col_comments where owner='NEWDSS' and table_name='%s'"%row_table[0])
+
+    for row_col in curs_col.fetchall():
+        write_line='alter table %s rename column %s to "%s";\n'%(row_table[0],row_col[0],row_col [1])
+        ctlfile.write(write_line)
+ctlfile.close()
+conn.close()
+  
+
