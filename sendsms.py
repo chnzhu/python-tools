8beb67736cc60bcfb2746ad7426112d21b38fe5e
chnzhu <33563118+chnzhu@users.noreply.github.com>
2020-05-29 19:32:41 +0800

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -2,7 +2,7 @@ from ZSI.client import NamedParamBinding as NPBinding
 import sys
 from ZSI.client import AUTH
 
-b=NPBinding(url='http://21.96.51.66:8080/axis/services/SendSms?wsdl',tracefile=sys.stdout)
+b=NPBinding(url='http://:8080/axis/services/SendSms?wsdl',tracefile=sys.stdout)
 b.SetAuth(AUTH.httpbasic,'sendsms','')
 message='kkkkkkkkk'
 a=message.decode('GBK').encode('UTF-8')
